c69f2eb80775737a1749d258a11e21c629df9f66
DDODivinePower <148576628+DDODivinePower@users.noreply.github.com>
2023-10-22 16:16:19 -0700

    Update Mission_3.py
    Updated Mission_3 program.

--- Team/Mission_3.py
+++ Team/Mission_3.py
@@ -1,29 +1,43 @@
+# IMPORTS
+# =======
 from pybricks.hubs import InventorHub
 from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor
 from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch
 
+
+# VARIABLE (HUB)
+# ==============
 hub = InventorHub()
 
-hub = InventorHub()
+# VARIABLES (DRIVING MOTORS + DRIVEBASE)
+# ==============================
 left_motor = Motor(port=Port.D, positive_direction=Direction.COUNTERCLOCKWISE)
 right_motor = Motor(port=Port.C)
-left_attachent_motor = Motor(port=Port.B)
-right_attachent_motor = Motor(port=Port.A)
-right_color_sensor = ColorSensor(Port.E)
-left_color_sensor = ColorSensor(Port.F)
 drivebase = DriveBase(left_motor, right_motor, 56, 110)
 
+# VARIABLES (ATTACHMENT MOTORS)
+# =============================
+left_attachent_motor = Motor(port=Port.B)
+right_attachent_motor = Motor(port=Port.A)
 
-def line_track():
+# VARIABLES (SENSORS)
+# ===================
+right_color_sensor = ColorSensor(Port.E)
+left_color_sensor = ColorSensor(Port.F)
+
+
+# FUNCTIONS
+# =========
+def follow_the_line():
     if (left_color_sensor.color() == Color.NONE) or (right_color_sensor.color() == Color.NONE):
-        # checks if the color detected is black and moves forward if it is
+        # Checks if the color detected is black and moves forward if it is
         left_motor.run_angle(speed=-700, rotation_angle=30, then=Stop.HOLD, wait=False)
         right_motor.run_angle(speed=-700, rotation_angle=30, then=Stop.HOLD, wait=False)
     else:
         while (left_color_sensor.color() != Color.NONE) or (right_color_sensor.color() != Color.NONE):
-            # if there is no black then go left and then if still no black then it movex right
+            # If there is no black then go left and then if still no black then it moves right
             left_motor.run_angle(speed=-700, rotation_angle=10, then=Stop.HOLD, wait=False)
             if (left_color_sensor.color() != Color.NONE) or (right_color_sensor.color() != Color.NONE):
                 while (left_color_sensor.color() != Color.NONE) or (right_color_sensor.color() != Color.NONE):
@@ -32,25 +46,43 @@ def line_track():
                         left_motor.run_angle(speed=-700, rotation_angle=30, then=Stop.HOLD, wait=False)
                         right_motor.run_angle(speed=-700, rotation_angle=30, then=Stop.HOLD, wait=False)
 
-def Mission():
-    drivebase.settings(straight_speed=100, straight_acceleration=400, turn_rate=80, turn_acceleration=200)
-    drivebase.straight(distance=150, then=Stop.HOLD, wait=True)
-    drivebase.turn(angle=-45, then=Stop.HOLD, wait=True)
-    drivebase.straight(distance=410, then=Stop.HOLD, wait=True)
+
+def solve_mission_3():
+    drivebase.settings(straight_speed=100, straight_acceleration=100, turn_rate=80, turn_acceleration=85)
+    # Drive from the home area to mission 2
+    drivebase.straight(distance=610, then=Stop.HOLD, wait=True)
+    # Turn to the right to the mission
     drivebase.turn(angle=90, then=Stop.HOLD, wait=True)
-    drivebase.straight(distance=580, then=Stop.HOLD, wait=True)
+    # Drive in front of Mission 3
+    drivebase.straight(distance=560, then=Stop.HOLD, wait=True)
+    # Turn left to face Mission 3
     drivebase.turn(angle=-90, then=Stop.HOLD, wait=True)
-    drivebase.straight(distance=140, then=Stop.HOLD, wait=True)
+    # Wait for half a second to stabilize
     wait(500)
+    # Jerk preperation
+    drivebase.turn(1)
+    # Drive into Mission 3
+    # It jerks to the left
+    drivebase.straight(distance=100, then=Stop.HOLD, wait=True)
+    # Stabilize
+    wait(500)
+    # Turn left to finish the mission
     drivebase.turn(angle=-40, then=Stop.HOLD, wait=True)
+    # Stabilize
     wait(500)
-    drivebase.turn(angle=40, then=Stop.HOLD, wait=True)
-    
+    # Turn right (back)
+    drivebase.turn(angle=20, then=Stop.HOLD, wait=True)
+
+
 def return_to_base():
-    drivebase.straight(distance=-590, then=Stop.HOLD, wait=True)
-    drivebase.turn(angle=-90, then=Stop.HOLD, wait=True)
-    drivebase.straight(distance=680, then=Stop.HOLD, wait=True)
+    drivebase.straight(distance=-110, then=Stop.HOLD, wait=True)
+    drivebase.turn(angle=30, then=Stop.HOLD, wait=True)
+    drivebase.straight(distance=-490, then=Stop.HOLD, wait=True)
+    drivebase.turn(angle=-120, then=Stop.HOLD, wait=True)
+    drivebase.straight(distance=660, then=Stop.HOLD, wait=True)
 
 
-Mission()
+# MAIN PROGRAM
+# ============
+solve_mission_3()
 return_to_base()
