1786235a24ea2bb9bdca27db23f0bc272a8f533a
Antoni Lương-Phạm Anh Quân <63928565+AntoniLuongPham@users.noreply.github.com>
2023-10-29 12:39:23 -0700

    Update Mission_1.py

--- Team/Mission_1.py
+++ Team/Mission_1.py
@@ -7,8 +7,8 @@ from pybricks.tools import wait, StopWatch
 hub = InventorHub()
 
 hub = InventorHub()
-left_motor = Motor(port=Port.D)
-right_motor = Motor(port=Port.C, positive_direction=Direction.COUNTERCLOCKWISE)
+left_motor = Motor(port=Port.D, positive_direction=Direction.COUNTERCLOCKWISE)
+right_motor = Motor(port=Port.C)
 left_attachent_motor = Motor(port=Port.B)
 right_attachent_motor = Motor(port=Port.A)
 right_color_sensor = ColorSensor(Port.E)
@@ -33,14 +33,10 @@ def line_track():
                         right_motor.run_angle(speed=-700, rotation_angle=30, then=Stop.HOLD, wait=False)
 
 def push():
-    drivebase.settings(straight_speed=100, straight_acceleration=200, turn_rate=800, turn_acceleration=1000)
-    drivebase.straight(distance=-20, then=Stop.HOLD, wait=True)
-    right_attachent_motor.run_angle(speed=100, rotation_angle=130, then=Stop.HOLD, wait=True)
-    wait(300)
-    drivebase.turn(angle=-35, then=Stop.HOLD, wait=True)
-    wait(500)
-    drivebase.turn(angle=35, then=Stop.HOLD, wait=True)
-    right_attachent_motor.run_angle(speed=300, rotation_angle=-130, then=Stop.HOLD, wait=True)
-    drivebase.straight(distance=20, then=Stop.HOLD, wait=True)
+    drivebase.settings(straight_speed=100, straight_acceleration=200, turn_rate=300, turn_acceleration=1000)
+    drivebase.straight(distance=100, then=Stop.HOLD, wait=True)
+    drivebase.turn(angle=80, then=Stop.HOLD, wait=True)
+    drivebase.turn(angle=-80, then=Stop.HOLD, wait=True)
+    drivebase.straight(distance=-100, then=Stop.HOLD, wait=True)
 
 push()  
