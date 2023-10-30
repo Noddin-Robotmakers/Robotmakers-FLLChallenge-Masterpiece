4a312c4e0ac232b01e8400de6a57ce4b66a9537d
Nathansbraincellsequallsnone <148728822+Nathansbraincellsequallsnone@users.noreply.github.com>
2023-10-29 17:25:35 -0700

    Update Innovation Project Goggles.py
    Code to the sensor.

--- Team/Innovation Project Goggles.py
+++ Team/Innovation Project Goggles.py
@@ -1,12 +1,18 @@
 from pybricks.hubs import PrimeHub
-from pybricks.pupdevices import Motor, UltrasonicSensor
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
 from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch
 
 hub = PrimeHub()
-distance = UltrasonicSensor(port=Port.A)
+
+distance_sensor = UltrasonicSensor(port=Port.A)
 
 while True:
-    if distance.distance() <= 50:
+    if distance_sensor.distance() <= 1000:  # When senses somthing within 1 meter
+        hub.speaker.volume(100)
+        hub.speaker.beep(500, 100)
+
+    elif distance_sensor.distance() <= 2000:  # When senses somthing over 1 meter
+        hub.speaker.volume(25)
         hub.speaker.beep(500, 100)
