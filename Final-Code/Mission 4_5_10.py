e0260174fb367144ecce01bfe2f2bbd3ed6d3a4b
Ariana Luong Pham <aria.luongpham@gmail.com>
2023-12-10 13:56:26 -0800

    Removed the line tracking from Mission 4_5_10_

--- Final-Code/Mission 4_5_10.py
+++ Final-Code/Mission 4_5_10.py
@@ -38,21 +38,7 @@ left_color_sensor = ColorSensor(Port.F)
 
 # FUNCTIONS
 # =========
-def follow_the_line():
-    if (left_color_sensor.color() == Color.NONE) or (right_color_sensor.color() == Color.NONE):
-        # Checks if the color detected is black and moves forward if it is
-        left_motor.run_angle(speed=-700, rotation_angle=30, then=Stop.HOLD, wait=False)
-        right_motor.run_angle(speed=-700, rotation_angle=30, then=Stop.HOLD, wait=False)
-    else:
-        while (left_color_sensor.color() != Color.NONE) or (right_color_sensor.color() != Color.NONE):
-            # If there is no black then go left and then if still no black then it moves right
-            left_motor.run_angle(speed=-700, rotation_angle=10, then=Stop.HOLD, wait=False)
-            if (left_color_sensor.color() != Color.NONE) or (right_color_sensor.color() != Color.NONE):
-                while (left_color_sensor.color() != Color.NONE) or (right_color_sensor.color() != Color.NONE):
-                    right_motor.run_angle(speed=-700, rotation_angle=10, then=Stop.HOLD, wait=False)
-                    if (left_color_sensor.color() == Color.NONE) or (right_color_sensor.color() == Color.NONE):
-                        left_motor.run_angle(speed=-700, rotation_angle=30, then=Stop.HOLD, wait=False)
-                        right_motor.run_angle(speed=-700, rotation_angle=30, then=Stop.HOLD, wait=False)
+
 
 def solve_mission_4():
     drivebase.settings(straight_speed=900,
