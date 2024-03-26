887705d9a81a13aeee35f150b5186d3a8e7d3465
Antoni Lương Phạm Anh Quân <Antoni.LuongPham@GMail.com>
2024-03-26 16:09:45 -0700

    Refactor M1

--- Final-Code/Mission_1.py
+++ Final-Code/Mission_1.py
@@ -6,59 +6,58 @@ from pybricks.parameters import Direction, Port, Stop
 from pybricks.robotics import DriveBase
 
 
-# CONSTANTS
-# =========
+# CONSTANTS for Robot Operation Parameters
+# ========================================
 STRAIGHT_SPEED = 900
 STRAIGHT_ACC = 900
 TURN_RATE = 900
 TURN_ACC = 300
 
 
-# VARIABLE (HUB)
-# ==============
+# VARIABLES Representing Robot Electronics
+# ========================================
+
+# Hub
 hub = InventorHub()
 
-# VARIABLES (DRIVING MOTORS + DRIVEBASE)
-# ======================================
+# Driving Motors & Drivebase
 left_motor = Motor(port=Port.D, positive_direction=Direction.COUNTERCLOCKWISE)
 right_motor = Motor(port=Port.C)
 drivebase = DriveBase(left_motor, right_motor, 56, 110)
 
-# VARIABLES (ATTACHMENT MOTORS)
-# =============================
+# Attachment Motors
 left_attachent_motor = Motor(port=Port.B)
 right_attachent_motor = Motor(port=Port.A)
 
-# VARIABLES (SENSORS)
-# ===================
+# Sensors
 right_color_sensor = ColorSensor(Port.E)
 left_color_sensor = ColorSensor(Port.F)
 
 
-# FUNCTIONS
-# =========
+# FUNCTIONS Representing Robot Behaviors
+# ======================================
 
-
-# Solve mission 1
-# ---------------
-def solve_and_return_to_base():
+# Solve Mission #1 and Return to Base
+def solve_m1_and_return_to_base():
     # Settings for drivebase
     drivebase.settings(straight_speed=STRAIGHT_SPEED,
                        straight_acceleration=STRAIGHT_ACC,
                        turn_rate=TURN_RATE,
                        turn_acceleration=TURN_ACC)
-    # Drive forward
-    drivebase.straight(distance=400, then=Stop.HOLD, wait=True)
-     # Drive forward
-    drivebase.straight(distance=-40, then=Stop.HOLD, wait=True)
-     # Drive forward
-    drivebase.straight(distance=60, then=Stop.HOLD, wait=True)
-    drivebase.turn(-15) 
 
     # Drive forward
+    drivebase.straight(distance=400, then=Stop.HOLD, wait=True)
+    # Drive backward
+    drivebase.straight(distance=-40, then=Stop.HOLD, wait=True)
+    # Drive forward
+    drivebase.straight(distance=60, then=Stop.HOLD, wait=True)
+    # Turn
+    drivebase.turn(-15)
+
+    # Drive backward to base
     drivebase.straight(distance=-400, then=Stop.HOLD, wait=True)
 
 
 # MAIN PROGRAM
 # ============
-solve_and_return_to_base()
+solve_m1_and_return_to_base()
