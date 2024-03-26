60fba91db0726dab1c0c7a933e050973359a405c
Antoni Lương Phạm Anh Quân <Antoni.LuongPham@GMail.com>
2024-03-26 16:44:41 -0700

    Refactor Missions #9 and #8

--- Final-Code/Mission_9_8.py
+++ Final-Code/Mission_9_8.py
@@ -1,57 +1,60 @@
 # IMPORTS
 # =======
 from pybricks.hubs import InventorHub
-from pybricks.pupdevices import Motor, ColorSensor
+from pybricks.pupdevices import Motor
 from pybricks.parameters import Direction, Port, Stop
 from pybricks.robotics import DriveBase
-from pybricks.tools import wait
 
 
-# CONSTANTS
-# =========
+# CONSTANTS for Robot Operation Parameters
+# ========================================
 STRAIGHT_SPEED = 101
 STRAIGHT_ACC = 101
 TURN_RATE = 70
 TURN_ACC = 80
 
 
-# VARIABLE (HUB)
-# ==============
+# VARIABLES Representing Robot Electronics
+# ========================================
+
+# Hub
 hub = InventorHub()
 
-# VARIABLES (DRIVING MOTORS + DRIVEBASE)
-# ======================================
+# Driving Motors & Drivebase
 left_motor = Motor(port=Port.D, positive_direction=Direction.COUNTERCLOCKWISE)
 right_motor = Motor(port=Port.C)
 drivebase = DriveBase(left_motor, right_motor, 56, 110)
 
-# VARIABLES (ATTACHMENT MOTORS)
-# =============================
+# Attachment Motors
 left_attachent_motor = Motor(port=Port.B)
 right_attachent_motor = Motor(port=Port.A)
 
-# FUNCTIONS
-# =========
 
-# Solve mission 3
-# ---------------
-def solve_mission_9_8():
+# FUNCTIONS Representing Robot Behaviors
+# ======================================
+
+# Solve missions #9 and #8
+def solve_missions_9_8():
+    # Set initial drivebase speed
     drivebase.settings(straight_speed=600,
                        straight_acceleration=400,
                        turn_rate=TURN_RATE,
                        turn_acceleration=TURN_ACC)
+
     # Go forward
     drivebase.straight(distance=40, then=Stop.HOLD, wait=True)
     # Go backward
     drivebase.straight(distance=-40, then=Stop.HOLD, wait=True)
-    # Change drivebase speed                   
+    # Change drivebase speed
     drivebase.settings(straight_speed=STRAIGHT_SPEED,
                        straight_acceleration=STRAIGHT_ACC,
                        turn_rate=TURN_RATE,
                        turn_acceleration=TURN_ACC)
+
     # Go forward
     drivebase.straight(distance=840, then=Stop.HOLD, wait=True)
 
-# Main program
-# ------------
-solve_mission_9_8()
+
+# MAIN PROGRAM
+# ============
+solve_missions_9_8()
