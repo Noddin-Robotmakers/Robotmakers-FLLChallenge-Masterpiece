fdd3946024c7c97b447811e792f3cd95d32b0784
Antoni Lương Phạm Anh Quân <Antoni.LuongPham@GMail.com>
2024-03-26 16:15:47 -0700

    Refactor mission 2

--- Final-Code/Mission_2.py
+++ Final-Code/Mission_2.py
@@ -6,57 +6,60 @@ from pybricks.parameters import Direction, Port, Stop
 from pybricks.robotics import DriveBase
 
 
-# CONSTANTS
-# =========
+# CONSTANTS for Robot Operation Parameters
+# ========================================
 STRAIGHT_SPEED = 100
 STRAIGHT_ACC = 100
 TURN_RATE = 100
 TURN_ACC = 80
 
 
-# VARIABLE (HUB)
-# ==============
+# VARIABLES Representing Robot Electronics
+# ========================================
+
+# Hub
 hub = InventorHub()
 
-# VARIABLES (DRIVING MOTORS + DRIVEBASE)
-# ======================================
+# Driving Motors & Drivebase
 left_motor = Motor(port=Port.D, positive_direction=Direction.COUNTERCLOCKWISE)
 right_motor = Motor(port=Port.C)
 drivebase = DriveBase(left_motor, right_motor, 56, 110)
 
-# VARIABLES (ATTACHMENT MOTORS)
-# =============================
+# Attachment Motors
 left_attachent_motor = Motor(port=Port.B)
 right_attachent_motor = Motor(port=Port.A)
 
-# VARIABLES (SENSORS)
-# ===================
+# Sensors
 right_color_sensor = ColorSensor(Port.E)
 left_color_sensor = ColorSensor(Port.F)
 
 
-# FUNCTIONS
-# =========
+# FUNCTIONS Representing Robot Behaviors
+# ======================================
 
-
-# Solve mission two
-# -----------------
-def solve_mission_two_and_return_to_base():
+# Solve Mission #2
+def solve_m2():
     # Settings for drivebase
     drivebase.settings(straight_speed=STRAIGHT_SPEED,
                        straight_acceleration=STRAIGHT_ACC,
                        turn_rate=TURN_RATE,
                        turn_acceleration=TURN_ACC)
+
     # Drive forward
     drivebase.straight(distance=600, then=Stop.HOLD, wait=True)
+
+
+# Return to Base
+def return_to_base():
     # Drive backward
     drivebase.straight(distance=-200, then=Stop.HOLD, wait=True)
-    # Turn
+    # Turn to face base
     drivebase.turn(angle=30, then=Stop.HOLD, wait=True)
-    # Drive backward
+    # Drive backward to go Home
     drivebase.straight(distance=-800, then=Stop.HOLD, wait=True)
 
 
 # MAIN PROGRAM
 # ============
-solve_mission_two_and_return_to_base()
+solve_m2()
+return_to_base()
