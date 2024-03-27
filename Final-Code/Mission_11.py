78766d7d9b47b05c4461bb04084543f26d698a02
Antoni Lương Phạm Anh Quân <Antoni.LuongPham@GMail.com>
2024-03-26 17:16:07 -0700

    Refactor M11

--- Final-Code/Mission_11.py
+++ Final-Code/Mission_11.py
@@ -6,63 +6,82 @@ from pybricks.parameters import Direction, Port, Stop
 from pybricks.robotics import DriveBase
 
 
-# CONSTANTS
-# =========
+# CONSTANTS for Robot Operation Parameters
+# ========================================
 STRAIGHT_SPEED = 900
 STRAIGHT_ACC = 300
 TURN_RATE = 80
 TURN_ACC = 85
 
 
-# VARIABLE (HUB)
-# ==============
+# VARIABLES Representing Robot Electronics
+# ========================================
+
+# Hub
 hub = InventorHub()
 
-# VARIABLES (DRIVING MOTORS + DRIVEBASE)
-# ======================================
+# Driving Motors & Drivebase
 left_motor = Motor(port=Port.D, positive_direction=Direction.COUNTERCLOCKWISE)
 right_motor = Motor(port=Port.C)
 drivebase = DriveBase(left_motor, right_motor, 56, 110)
 
-# VARIABLES (ATTACHMENT MOTORS)
-# =============================
+# Attachment Motors
 left_attachent_motor = Motor(port=Port.B)
 right_attachent_motor = Motor(port=Port.A)
 
-# VARIABLES (SENSORS)
-# ===================
+# Sensors
 right_color_sensor = ColorSensor(Port.E)
 left_color_sensor = ColorSensor(Port.F)
 
 
-# FUNCTIONS
-# =========
+# FUNCTIONS Representing Robot Behaviors
+# ======================================
 
-# solve mission 13
-# ----------------
-def solve_mission_13_and_return_to_base():
+# solve mission 11 and return to other Base
+def solve_mission_11_and_return_to_base():
     # Settings for drivebase
     drivebase.settings(straight_speed=STRAIGHT_SPEED,
                        straight_acceleration=STRAIGHT_ACC,
                        turn_rate=TURN_RATE,
                        turn_acceleration=TURN_ACC)
+
+    # Go forward
     drivebase.straight(200)
+    # Turn
     drivebase.turn(-90)
+    # Go forward
     drivebase.straight(700)
+    # Turn
     drivebase.turn(95)
+    # Go forward
     drivebase.straight(155)
+    # Do M11
     right_attachent_motor.run_angle(speed=1000, rotation_angle=1650,
                                     then=Stop.COAST, wait=True)
+
+
+# Return to other home Base
+def return_to_base():
+
+    # Go forward
     drivebase.straight(-155)
+    # Turn
     drivebase.turn(-90)
+    # Go forward
     drivebase.straight(300)
+    # Turn
     drivebase.turn(-90)
+    # Go forward
     drivebase.straight(200)
+    # Turn
     drivebase.turn(-90)
+    # Go forward
     drivebase.straight(200)
+    # Turn
     drivebase.turn(-90)
 
 
 # MAIN PROGRAM
 # ============
-solve_mission_13_and_return_to_base()
+solve_mission_11_and_return_to_base()
+return_to_base()
