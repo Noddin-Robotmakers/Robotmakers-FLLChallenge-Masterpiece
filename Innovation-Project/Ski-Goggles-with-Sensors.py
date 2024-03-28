2363cd89100610017a65b2caf9ae40067e3e6ec3
Ariana Lương Phạm <Aria.LuongPham@gmail.com>
2024-03-28 10:02:34 -0700

    move Innovation-Project/Ski-Goggles-with-Sensors.py code file

--- Innovation-Project/Ski-Goggles-with-Sensors.py
+++ Innovation-Project/Ski-Goggles-with-Sensors.py
@@ -7,12 +7,12 @@ hub = PrimeHub()
 distance_sensor = UltrasonicSensor(port=Port.A)
 
 while True:
-    # When senses somthing within 1 meter
+    # when sensing something within 1 meter away
     if distance_sensor.distance() <= 1000:
         hub.speaker.volume(100)
         hub.speaker.beep(500, 100)
 
-    # When senses somthing over 1 meter
+    # when sensing something over 1 meter away
     elif distance_sensor.distance() <= 2000:
         hub.speaker.volume(25)
         hub.speaker.beep(500, 100)
